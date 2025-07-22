0b812498609046e0cfe7a226f83466e63247e30e
Balázs Marcell <KoegLike@proton.me>
2025-07-22 15:23:40 +0300

    got stuck on multiplayer

---
+++ scripts/main_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@onready var name_field: TextEdit = $VBoxContainer/Name
+
+
+func _on_name_text_changed() -> void:
+	Manager.player_info.name=name_field.text
+
+
+func _on_host_pressed() -> void:
+	Manager.create_game()
+
+
+func _on_join_pressed() -> void:
+	Manager.join_game()
+
+
+func _on_start_pressed() -> void:
+	Manager.load_game("res://scenes/main.tscn")
