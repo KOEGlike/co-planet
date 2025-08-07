03fbafe9b39dd10d022fae2594bf4c3743227772
Balázs Marcell <KoegLike@proton.me>
2025-08-07 22:13:14 +0300

    Fix signaling server URL for WebSocket connection

--- multiplayer/ws_webrtc_client.gd
+++ multiplayer/ws_webrtc_client.gd
@@ -20,7 +20,7 @@ var code := 1000
 var reason := "Unknown"
 var old_state := WebSocketPeer.STATE_CLOSED
 
-const SIGNALING_SERVER_URL = "co-planet-signaling.koeg.dev "
+const SIGNALING_SERVER_URL = "wss://co-planet-signaling.koeg.dev"
 
 func connect_to_url(url: String = SIGNALING_SERVER_URL) -> void:
 	close()
